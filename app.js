9560a0c72c1e5fac46b593f5803a6ec73c7f4221
M Moazam <muhammadmoazzamshigri@gmail.com>
2024-04-02 14:27:37 +0500

    Uploaded

---
+++ app.js
@@ -0,0 +1,53 @@
+let timerDisplay = document.querySelector(".timerDisplay");
+let stopButton = document.querySelector('#stop');
+let startButton = document.querySelector('#start');
+let resetButton = document.querySelector('#reset');
+
+let mSec = 0;
+let sec  = 0;
+let min  = 0;
+
+let timerId = null;
+
+let startTimer = () => {
+ 
+        
+        mSec++;
+        if(mSec==10){
+          mSec=0;
+          sec++;
+        }
+        if(sec==60){
+          sec++;
+          min++;
+        }
+        let mSecString = mSec<10?`0${mSec}`:mSec;
+        let secString = sec<10?`0${sec}`:sec;
+        let minString = min<10?`0${min}`:min;
+        timerDisplay.innerHTML = `${minString} : ${secString} : ${mSecString}`
+
+  }
+
+  startButton.addEventListener('click',() => {
+    if(timerId!=null){
+        clearInterval(timerId);// it stops the set interval function
+    }
+    timerId = setInterval(startTimer,100);
+  }
+  )
+  stopButton.addEventListener('click',() => {
+   
+        clearInterval(timerId);
+    
+    
+  });
+  resetButton.addEventListener('click',() => {
+   
+        clearInterval(timerId);
+        timerDisplay.innerHTML = "00 : 00 : 00";
+        mSec = 0;
+        min=0;
+        sec=0;
+    
+    
+  });
